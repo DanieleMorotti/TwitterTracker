fac2fb5206709fd45c4245f27368d5b0a32fe3c2
HowlHowlHowl <gabrielefogu@gmail.com>
2020-10-22 19:17:12 +0200

    Stream attivo per una sola parola, bisogna pulire lo stream in modo da poterne cercare una diversa dopo la prima

--- application.py
+++ application.py
@@ -23,13 +23,57 @@ def get_tweets(api, username):
         lista['tweets'].append({'testo':tweet.text,'user':tweet.user.name,'data':tweet.created_at.strftime('%m/%d/%Y')})
         
     return json.dumps(lista,indent=2, sort_keys=True)
+# Get tweets by keyword
+#override tweepy.StreamListener to add logic to on_status
 
+streaming_data = []
+
+class MyStreamListener(tweepy.StreamListener):
+    def on_connect(self):
+        print("Connected to the server!")
+    
+    def on_status(self, status):
+        text = "";
+        if hasattr(status, "retweeted_status"): # Check if Retweet
+            try:
+                text = status.retweeted_status.extended_tweet["full_text"]
+            except AttributeError:
+                text = status.retweeted_status.text
+        else:
+            try:
+                text = status.extended_tweet["full_text"]
+            except AttributeError:
+                text = status.text
+        
+        streaming_data.append(text)
+
+    def on_error(self, status):
+        print('Streaming Error Status Code - ' + status)
+
+myStreamListener = MyStreamListener()
+# Function to starm StreamListener
+def get_keyword_stream(keyword):
+    global myStreamListener
+    myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener, tweet_mode = 'extended')
+    myStream.filter(track=[keyword], is_async=True)
+
+    return 'Streaming started'
 
 # Route for the index page
 @application.route('/getTweets')
 def sendData():
     return get_tweets(api,'SpaceX')
-    
+
+# Route to start stream
+@application.route('/searchTweets/<keyword>')
+def getStreamingListener(keyword):
+    return get_keyword_stream(str(keyword))
+
+# Route to get tweets from the stream
+@application.route('/streamUpdate')
+def getStreamingTweets():
+    return json.dumps(streaming_data)        
+
 @application.route('/')
 def getPage():
     return send_file("index.html")
