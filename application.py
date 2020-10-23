457c634ac5e1c9b109143e5b18679fdc276787a1
Dario Mylonopoulos <dario.mylonopoulos@studio.unibo.it>
2020-10-23 19:25:51 +0200

    Rinominate route e aggiornato index

--- application.py
+++ application.py
@@ -4,31 +4,38 @@ import tweepy
 import datetime, time
 import json
 
+# Twitter keys
 API_KEY = 'j6DEP35tCTAxgmoaCYMQAThHw'
 API_SECRET_KEY = 'IStBpbpGzInMyzxAtlg6ZRLpJWZu5hS0SUAm6RB8YdmlNLz9OD'
 ACCESS_TOKEN = '1315934369433956354-KSJwtOBr3B9deMyqlF2pkNBU3sTmnk'
 ACCESS_SECRET_TOKEN = 'qLaGNPiYvJX0RgWeqvdx9iDe5lwDQRRAe5OruhGLtHkq1'
 
+# Initialize tweepy
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET_TOKEN)
 api = tweepy.API(auth)
 
+# Initialize flask
 application = Flask(__name__)
 
-def get_tweets(api, username):
-    deadend = False
-    lista = {'tweets':[]}
-    tweets = api.user_timeline(username)
-    for tweet in tweets:
-        lista['tweets'].append({'testo':tweet.text,'user':tweet.user.name,'data':tweet.created_at.strftime('%m/%d/%Y')})
-        
+# def get_tweets(api, username):
+#     deadend = False
+#     lista = {'tweets':[]}
+#     tweets = api.user_timeline(username)
+#     for tweet in tweets:
+#         lista['tweets'].append({'testo':tweet.text,'user':tweet.user.name,'data':tweet.created_at.strftime('%m/%d/%Y')})
+#         
+# 
+#     string = json.dumps(lista, ensure_ascii=False, indent=2, sort_keys=True);
+#     return string
+# 
+#    
+# @application.route('/getTweets')
+# def sendData():
+#     return get_tweets(api,'SpaceX')
 
-    string = json.dumps(lista, ensure_ascii=False, indent=2, sort_keys=True);
-    return string
 
-streaming_data = []
-
-
+#Stream tweets
 class MyStreamListener(tweepy.StreamListener):
     def on_connect(self):
         print("Connected to the server!")
@@ -46,26 +53,56 @@ class MyStreamListener(tweepy.StreamListener):
             except AttributeError:
                 text = status.text
         
-        streaming_data.append(text)
+        streaming_data.append({'text': text, 'user':status.user.name,'data':status.created_at.strftime('%m/%d/%Y')})
 
     def on_error(self, status):
         print('Streaming Error Status Code - ' + status)
 
 myStreamListener = MyStreamListener()
 myStream = None
-# Function to starm StreamListener
-def get_keyword_stream(keyword):
-    global myStreamListener
-    global myStream
-    global streaming_data
+streaming_data = []
+
+# Disconnect current stream if there is one
+def stopStreamListener():
     if(myStream != None): 
         myStream.disconnect()
-        streaming_data = []
+        streaming_data.clear()
+    
+# Function to starm StreamListener
+def startStreamListener(keyword):
+    global myStreamListener
+    global myStream
+    
+    # Disconnect existing stream
+    stopStreamListener()
+    
+    # Create a new stream with the specified 
     myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener, tweet_mode = 'extended')
     myStream.filter(track=[keyword], is_async=True)
 
-    return 'Streaming started'
+# Route to start stream
+@application.route('/streamStart')
+def streamStart():
+    word = request.args.get("word")
+    if not word:
+        return Response(status = 400)
+    
+    startStreamListener(word)
+    
+    return Response(status = 200)
+    
+# Route to stop stream
+@application.route('/streamStop')
+def streamStop():
+    stopStreamListener()
+    return Response(status = 200)
 
+# Route to get tweets from the stream
+@application.route('/streamUpdate')
+def streamUpdate():
+    return Response(json.dumps(streaming_data, ensure_ascii=False, indent=2), status=200, mimetype="application/json");
+    
+#Search tweets
 def get_tweet_text(tweet):
     try:
        text = tweet.retweeted_status.full_text
@@ -73,13 +110,12 @@ def get_tweet_text(tweet):
        text = tweet.full_text
     
     return text
-
                 
 @application.route('/search')
 def search():
     word = request.args.get("word");
     if not word:
-        return ""
+        return Response(status = 400);
     
     list = [];
     for tweet in api.search(q=word, tweet_mode="extended"):
@@ -87,24 +123,8 @@ def search():
     
     return Response(json.dumps(list, ensure_ascii=False, indent=2), status=200, mimetype="application/json");
     
-    
+   
 # Route for the index page
-@application.route('/getTweets')
-def sendData():
-    return get_tweets(api,'SpaceX')
-
-# Route to start stream
-@application.route('/searchTweets/<keyword>')
-def getStreamingListener(keyword):
-    return get_keyword_stream(str(keyword))
-
-# Route to get tweets from the stream
-@application.route('/streamUpdate')
-def getStreamingTweets():
-    return json.dumps(streaming_data) 
-    return Response(json.dumps(streaming_data, ensure_ascii=False, indent=2), status=200, mimetype="application/json");
-    
-
 @application.route('/')
 def getPage():
     return send_file("index.html")
