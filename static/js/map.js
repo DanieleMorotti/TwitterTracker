6c6c8c021c134b0d374e867d2fac6499de4d6ec5
DaniMoro <daniele.morotti.99@gmail.com>
2020-11-17 13:35:49 +0100

    Aggiunta funzione per disegnare zona di ricerca con un cerchio

--- static/js/map.js
+++ static/js/map.js
@@ -1,25 +1,82 @@
 var map = null;
 var search_area = null;
+//saving the last shape i drew, so when I draw a new shape I remove the previous one
+let shape = null;
 //Function provided by Google to initialize the map associated to the div w/ id="map"
 function initMap() {
 
-    var geocoder = new google.maps.Geocoder();
+    //TODO:serve questa variabile? var geocoder = new google.maps.Geocoder();
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 41.885453, lng: 12.498221 },
         zoom: 5,
     });
-    //Setup EventListener
-    setUpClickListener();
-}
-//Event listener for clicks on that writes coordinates in the coresponding field
-function setUpClickListener() {
-    map.addListener("click", (mapsMouseEvent) => {
-        getCoordinatesOnCLick(mapsMouseEvent);
+
+    //set up the drawing tool
+    const drawingManager = new google.maps.drawing.DrawingManager({
+        drawingMode: google.maps.drawing.OverlayType.MARKER,
+        drawingControl: true,
+        drawingControlOptions: {
+            //set the position of the tool and the shapes you can draw
+            position: google.maps.ControlPosition.TOP_CENTER,
+            drawingModes: [
+                google.maps.drawing.OverlayType.CIRCLE
+            ],
+        },
+        circleOptions: {
+            fillColor: "red",
+            fillOpacity: 0.3,
+            strokeWeight: 2,
+            clickable: false,
+            editable: true,
+            zIndex: 1,
+        },
+    });
+    drawingManager.setMap(map);
+
+
+    //after the end of the draw action
+    google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
+        //delete the previous shape drawn by the user and save the new one
+        if(shape)shape.setMap(null);
+        shape = event.overlay;
+        //delete the circle drawn automatically from the function @drawSearchAreaOnMap
+        if(search_area)search_area.setMap(null);
+
+        if (event.type == 'circle') {
+            //to remove the parenthesis from the coordinates string
+            let center = shape.getCenter().toString().replace(/[\(\)]/g,'');
+            let radius = shape.getRadius();
+            
+            $('#coordinates').val(center);
+            //because the range is in km, but the radius is in m
+            $('#radius').val(parseInt(radius/1000));
+
+            //if the radius > 1000km i set to 1000km
+            if(radius > 1000000) shape.setRadius(1000000);
+        }
+
+        /*The shape can be moved or resized, added eventlisteners to manage this actions*/
+
+        shape.addListener('radius_changed', function(e) {
+            let newRadius = shape.getRadius();
+            $('#radius').val(parseInt(newRadius/1000));
+            $("#radius").trigger( "custom", parseInt(newRadius/1000) );
+    
+            //if the radius > 1000km i set to 1000km
+            if(newRadius > 1000000) shape.setRadius(1000000);
+        });
+        shape.addListener('center_changed', function(e){
+            let newCenter = shape.getCenter().toString().replace(/[\(\)]/g,'');
+            $('#coordinates').val(newCenter);
+        });
     });
 }
-// Draw Searched area on the map
 
+// Draw Searched area on the map
 function drawSearchAreaOnMap(loc, r, color) {
+    //delete the circle drawn by the user
+    if(shape) shape.setMap(null);
+
     //initialized the radius of the circle and the center
     let radius = 1000 * r;// parseInt(loc.split(',')[2].slice(0, -2));
     let center = { lat: parseFloat(loc.split(',')[0]), lng: parseFloat(loc.split(',')[1]) };
@@ -28,7 +85,7 @@ function drawSearchAreaOnMap(loc, r, color) {
     if (search_area) {
         search_area.setMap(null);
     }
-    // Add the circle for this city to the map.
+    // Add the circle for these coordinates to the map.
     search_area = new google.maps.Circle({
         strokeColor: color,
         strokeOpacity: 0.8,
@@ -38,16 +95,23 @@ function drawSearchAreaOnMap(loc, r, color) {
         map,
         center: center,
         radius: radius,
+        editable:true
     });
-    search_area.addListener("click", (mapsMouseEvent) => {
-        getCoordinatesOnCLick(mapsMouseEvent);
+
+    /*The shape can be moved or resized, added eventlisteners to manage this actions*/
+    search_area.addListener('radius_changed', function(e) {
+        let newRadius = search_area.getRadius();
+        $('#radius').val(parseInt(newRadius/1000));
+
+        //if the radius > 1000km i set to 1000km
+        if(newRadius > 1000000) search_area.setRadius(1000000);
+    });
+    search_area.addListener('center_changed', function(e){
+        let newCenter = search_area.getCenter().toString().replace(/[\(\)]/g,'');
+        $('#coordinates').val(newCenter);
     });
 }
 
-function getCoordinatesOnCLick(mapsMouseEvent) {
-    $('#coordinates').val(mapsMouseEvent.latLng.lat().toFixed(2) + ', ' + mapsMouseEvent.latLng.lng().toFixed(2));
-    drawSearchAreaOnMap($('#coordinates').val(), $('#radius').val(), '#FF0000');
-}
 // Event associated on change of coordinates to draw the area
 $(document).on('input', '#coordinates', function () {
     let coordinates = $('#coordinates').val();
