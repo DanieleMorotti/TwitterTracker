c487dec3534ea299b8b715b8f01c93362adf2a63
erika.lena@outlook.it <erika.lena@outlook.it>
2020-12-05 12:36:17 +0100

    script wakatime

--- wakatime.py
+++ wakatime.py
@@ -3,10 +3,13 @@ import json
 import datetime
 import sys
 
-url = 'https://wakatime.com/share/@1e6c8175-74f3-46a2-ba63-420902c79a95/2ebb8805-f18f-46ab-a07f-e8e6769790f7.json'
+url = 'https://wakatime.com/share/@8068813f-7ab9-4ce6-9f99-41777a9d6ede/ffd8737f-cf3b-43c8-90e9-cbe61c04c7ce.json'
+url2 = 'https://wakatime.com/share/@8068813f-7ab9-4ce6-9f99-41777a9d6ede/95df2f63-6790-45e8-a43d-b96af58a6b1f.json'
 body = requests.get(url).json()
+languages =  requests.get(url2).json()
 
 datefrom = datetime.datetime.strptime("20/11/2020", "%d/%m/%Y")
+
 if len(sys.argv) > 1:
     try:
         datefrom = datetime.datetime.strptime(sys.argv[1], "%d/%m/%Y")
@@ -29,7 +32,9 @@ with open("wakatime.txt", "w") as f:
         date = dateobj.strftime("%d/%m/%Y")
         print(date + "\t" + time, file = f)
 
-
+    print('\n', file = f)
+    for l in languages["data"]:
+        print(l["name"] + "\t" + str(l["percent"]), file = f)
         
 
 
