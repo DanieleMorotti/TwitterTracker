89ba7034979a7fdd72e736388c745f7fca405df4
erika.lena@outlook.it <erika.lena@outlook.it>
2020-12-10 01:50:49 +0100

    home

--- static/js/comp_main.js
+++ static/js/comp_main.js
@@ -3,13 +3,53 @@ export default {
     name: 'main',
     template: `
         <div>
-            <img  src="/static/img/scooby_doo.png"  width="300" style="margin: 10rem">
+            <div class="main-container">
+                <div class="main-title-container">
+                    <h2>Fraydrum</h2>   
+                    <h5>Raccogli e analizza tweet</h5>
+                    <button @click="$('#toSearch').click();">Inizia</button>
+                </div>
+                <div class="lined-header-container">  
+                    <div class="lined-header">Raccolta e analisi</div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="dashboard-container center-block text-center">
+                            <img src="/static/img/map_icon.png" alt="icona mappa">
+                            <div class="title">Mappe</div>
+                            <div class="description">Visualizza i tweet raccolti sulla mappa</div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="dashboard-container center-block text-center">
+                            <img src="/static/img/cloud_icon.png" alt="icona word-cloud">
+                            <div class="title">Word-cloud</div>
+                            <div class="description">Disposizione delle parole in forma di word-cloud</div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="dashboard-container center-block text-center">
+                            <img src="/static/img/chart_icon.png" alt="icona grafici">
+                            <div class="title">Grafici</div>
+                            <div class="description">Visualizzazione grafica delle parole ricorrenti</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="lined-header-container">  
+                    <div class="lined-header">Condivisione</div>
+                </div>
+                <div class="row">
+                <div class="col-md-12">
+                    <div class="dashboard-container center-block text-center">
+                        <img src="/static/img/share_icon.png" alt="icona condivisione">
+                        <div class="title">Condivi</div>
+                        <div class="description">Condividi i dati raccolti su Twitter</div>
+                    </div>
+                </div>
+            </div>
+            </div>
         </div>
-    `,
-
-    methods: {
-    },
-
-    activated() {
-    }
+    `
 }
